fb9fa7fd3ee3be8ed63435638bda8155a4179aaf
Gabriel <gabriel@gabriel-PC>
2018-11-02 10:34:06 -0300

    reject

---
+++ resources/views/auth/info.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="container">
+        <section class="row">
+            <section class="col-md-12">
+                <form method="POST" action="{{ route('info') }}">
+
+                </form>
+            </section>
+        </section>
+    </section>
+@endsection
