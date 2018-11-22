7dba3cfdfee1dd8cbce6f05a7ba83167f872e695
Gabriel Lisboa <gabrielbento.rx@outlook.com>
2018-11-21 23:23:07 -0200

    Refactor Profile avatar upload controller to delete a old file when update an avatar

--- app/Http/Controllers/Web/ProfileAvatarUploadController.php
+++ app/Http/Controllers/Web/ProfileAvatarUploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Requests\ProfileAvatarUploadRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileAvatarUploadController extends Controller
 {
@@ -11,12 +12,22 @@ class ProfileAvatarUploadController extends Controller
     {
         $user = auth()->user();
 
-        $avatarName = "{$user->id}_avatar.{$request->avatar->getClientOriginalExtension()}";
+        $hash = str_random(12);
+        $avatarName = "{$user->id}_avatar_{$hash}.{$request->avatar->getClientOriginalExtension()}";
+
+        $this->deleteIfExists($request->old_path);
 
         $path = $request->avatar->storeAs('avatars', $avatarName, 'public');
 
         $user->profile()->update(['avatar' => $path]);
 
         return ['path' => $path];
+    }
+
+    private function deleteIfExists($old_path)
+    {
+        if (Storage::disk('public')->exists($old_path)) {
+            Storage::disk('public')->delete($old_path);
+        }
     }
 }
